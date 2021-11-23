5b2e689942b25871befb950d07e41476aee7f4ac
Molted <joshuaconcepcion12.jc@gmail.com>
2021-11-23 09:11:52 +0800

    social media application

---
+++ src/Model/Table/ArticlesTable.php
@@ -0,0 +1,48 @@
+<?php
+// src/Model/Table/ArticlesTable.php
+namespace App\Model\Table;
+
+use Cake\ORM\Table;
+// the Text class
+use Cake\Utility\Text;
+// the EventInterface class
+use Cake\Event\EventInterface;
+
+// add this use statement right below the namespace declaration to import
+// the Validator class
+use Cake\Validation\Validator;
+
+class ArticlesTable extends Table
+{
+    public function initialize(array $config): void
+    {
+        $this->addBehavior('Timestamp');
+    }
+
+    // Add the following method.
+
+    public function beforeSave(EventInterface $event, $entity, $options)
+    {
+        if ($entity->isNew() && !$entity->slug) {
+            $sluggedTitle = Text::slug($entity->title);
+            // trim slug to maximum length defined in schema
+            $entity->slug = substr($sluggedTitle, 0, 191);
+        }
+    }
+
+    // src/Model/Table/ArticlesTable.php
+
+    // Add the following method.
+    public function validationDefault(Validator $validator): Validator
+    {
+        $validator
+            ->notEmptyString('title')
+            ->minLength('title', 10)
+            ->maxLength('title', 255)
+
+            ->notEmptyString('body')
+            ->minLength('body', 10);
+
+        return $validator;
+    }
+}
